1281e0df7e898f872aebbd18ee4589b09eec7296
alitagpir <alitagpir@gmail.com>
2021-10-20 21:57:59 +0100

    All 3 practises are done

---
+++ script.js
@@ -0,0 +1,27 @@
+
+/*
+Practise 1
+let value= prompt("What is your number?")
+if (value %2===0){
+    alert("even number")
+}else{
+    alert("odd number")
+} 
+
+Practise 2
+let fNum= prompt("Give your first number")
+let sNum= prompt("Give your second number")
+if (fNum % sNum===0){
+    alert(`True ${fNum} is divisble by ${sNum}`)
+}else{
+    alert(`False ${fNum} is not divisble by ${sNum}`)
+} 
+
+Practise 3 approach A 
+let busCap= 42
+let students=344
+let busNum= Math.ceil(students/busCap)
+alert(`there will be ${busNum} busses need to take all ${students} to the theatre`)
+*/
+
+people = parseInt(prompt("student num"))
